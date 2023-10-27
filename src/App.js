d0158ab3999cec6086dee4e5292db8ae45311cba
Reza Safari <rsafari.s@gmail.com>
2023-10-27 11:11:35 -0500

    added basic board logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Chess } from "chess.js";
+import Chessboard from "chessboardjsx";
+import { useState } from "react";
 
 function App() {
+  const [game, setGame] = useState(new Chess());
+
+  const handleMove = ({ sourceSquare, targetSquare }) => {
+    try {
+      game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q",
+      });
+      setGame(new Chess(game.fen()));
+    } catch (error) {
+      alert("Invalid move!");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chessboard
+        position={game.fen()}
+        onDrop={handleMove}
+        width={400}
+        boardStyle={{
+          borderRadius: "5px",
+          boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`,
+        }}
+      />
     </div>
   );
 }

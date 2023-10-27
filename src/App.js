fc000a95bcdac73df63ac26ead37e8985f877407
Reza Safari <rsafari.s@gmail.com>
2023-10-27 14:09:47 -0500

    added yellow color for king square on check

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import { GameStatus } from "./components/GameStatus";
 
 function App() {
   const [game, setGame] = useState(new Chess());
+  const [kingPos, setKingPos] = useState({
+    w: "e1",
+    b: "e8",
+  });
 
   const handleMove = ({ sourceSquare, targetSquare }) => {
     try {
-      game.move({
+      const move = game.move({
         from: sourceSquare,
         to: targetSquare,
         promotion: "q",
       });
+      if (move.piece === "k") {
+        setKingPos({
+          ...kingPos,
+          [move.color]: move.to,
+        });
+      }
       setGame(new Chess(game.fen()));
     } catch (error) {
       console.log("Invalid move");
@@ -24,6 +34,16 @@ function App() {
     return screenWidth / 3;
   };
 
+  const getCheckKing = () => {
+    if (game.inCheck()) {
+      const turn = game.turn();
+      return kingPos[turn];
+    }
+    return null;
+  };
+
+  const checkedKing = getCheckKing();
+
   return (
     <div className="App">
       <div className="overlay-container">
@@ -31,6 +51,11 @@ function App() {
           position={game.fen()}
           onDrop={handleMove}
           calcWidth={calcBoardWidth}
+          squareStyles={{
+            [checkedKing]: {
+              backgroundColor: "yellow",
+            },
+          }}
           boardStyle={{
             borderRadius: "5px",
             boxShadow: `0 5px 15px rgba(0, 0, 0, 0.5)`,
